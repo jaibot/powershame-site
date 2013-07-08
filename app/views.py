0479c75aa078465dfd1f3196259655a56773bd69
Jai Dhyani <jai@jaibot.com>
2013-07-08 17:09:29 -0400

    testing facebook integration

--- app/views.py
+++ app/views.py
@@ -1,6 +1,14 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-        return "Hello, World!"
+    user = { 'nickname': 'Jai' } # fake user
+    return render_template("index.html",
+      title = 'Home',
+      user = user) 
+
+@app.route('/channel.html')
+def fb_channel():
+    return render_template("channel.html")
