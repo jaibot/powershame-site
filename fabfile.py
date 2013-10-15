67c71a7cf658636550826bd2ed58dd16f4574433
Jai Dhyani <jai@jaibot.com>
2013-10-14 20:26:58 -0400

    improved fab clean

--- fabfile.py
+++ fabfile.py
@@ -33,5 +33,4 @@ def deploy():
 def clean():
     #remove local build files
     local('rm -rf dist Powershame_Server.egg-info')
-def find_pyc():
-    local('find . -name "*.pyc"')
+    local('pyclean .')
