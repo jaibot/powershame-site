a1fcb9328db2785840ea00b8a88df49cdf5ca55e
Jai Dhyani <jai@jaibot.com>
2013-11-08 09:14:30 -0500

    cleanup :

--- powershame/police.py
+++
@@ -1,9 +0,0 @@
-from powershame import db
-
-def allow_new_upload_form( user=None, **kwargs ):
-    #TODO: police applications for new upload forms
-    return True
-
-def allow_new_client( user=None, **kwargs ):
-    #TODO: police applications for new clients
-    return True
