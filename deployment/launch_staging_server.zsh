4b54d93d6d547523db4d9e0ad85a6abd7b72a063
Jai Dhyani <jai@jaibot.com>
2014-01-06 17:38:24 -0500

    removed mysql from pip

---
+++ deployment/launch_staging_server.zsh
@@ -0,0 +1,1 @@
+aws ec2 run-instances --image-id ami-a73264ce --count 1 --instance-type m1.small --key-name jai-2013  --security-group-ids sg-a11badce --subnet-id subnet-6d36b10c --associate-public-ip-address
